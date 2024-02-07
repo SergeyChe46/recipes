579417d33a888d27975c0080a8629c48273b763d
Sergey Chevychelov <sergeyche46@mail.ru>
2024-02-07 14:20:17 +0300

    всплывает изображение с рецептом

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RecipesListComponent } from './components/recipes/recipes-list/recipes-list.component';
 import { RecipesItemComponent } from './components/recipes/recipes-item/recipes-item.component';
+import { RecipesListComponent } from './components/recipes/recipes-list/recipes-list.component';
 import { RemoveCommaPipe } from './services/pipes/remove-comma.pipe';
-import { ShowImageDirective } from './services/directives/show-image.directive';
 
 @NgModule({
-  declarations: [AppComponent, RecipesListComponent, RecipesItemComponent, RemoveCommaPipe, ShowImageDirective],
+  declarations: [
+    AppComponent,
+    RecipesListComponent,
+    RecipesItemComponent,
+    RemoveCommaPipe,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/services/directives/show-image.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ShowImageDirective } from './show-image.directive';
-
-describe('ShowImageDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ShowImageDirective();
-    expect(directive).toBeTruthy();
-  });
-});
