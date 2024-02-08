f2daa7f265f3116e10a29c04fb879617aec66145
Sergey Chevychelov <sergeyche46@mail.ru>
2024-02-08 20:31:18 +0300

    тултип двигается за курсором

--- src/app/components/recipes/recipes-item/recipes-item.component.ts
+++ src/app/components/recipes/recipes-item/recipes-item.component.ts
@@ -1,4 +1,12 @@
-import { Component, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostListener,
+  Input,
+  ViewChild,
+} from '@angular/core';
+import { fromEvent } from 'rxjs';
 import { IRecipe } from '../../../../models/recipe.interface';
 
 @Component({
@@ -6,6 +14,27 @@ import { IRecipe } from '../../../../models/recipe.interface';
   templateUrl: './recipes-item.component.html',
   styleUrl: './recipes-item.component.css',
 })
-export class RecipesItemComponent {
+export class RecipesItemComponent implements AfterViewInit {
   @Input() recipe: IRecipe | undefined;
+  @ViewChild('image') imageUrl!: ElementRef<HTMLDivElement>;
+  @ViewChild('recipeItem') recipeItem!: ElementRef<HTMLDivElement>;
+
+  private mouseMove = () =>
+    fromEvent(this.recipeItem.nativeElement, 'mousemove').subscribe({
+      next: (event: any) => {
+        this.imageUrl.nativeElement.style.visibility = 'visible';
+        let positionX = Math.max(event.clientX, event.layerX);
+        let positionY = Math.min(event.clientY, event.layerY);
+
+        this.imageUrl.nativeElement.style.top = String(positionY) + 'px';
+        this.imageUrl.nativeElement.style.left = String(positionX) + 'px';
+      },
+    });
+  @HostListener('mouseleave', ['$event']) onMouseLeave() {
+    this.imageUrl.nativeElement.style.visibility = 'hidden';
+  }
+
+  ngAfterViewInit(): void {
+    this.mouseMove();
+  }
 }
